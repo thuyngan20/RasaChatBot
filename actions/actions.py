0fa18cc40e0c06d4ba644d5d9a06414b52d35f6f
thuyngan20 <phanthuynganit@gmail.com>
2022-07-12 11:15:28 +0700

    init project

---
+++ actions/actions.py
@@ -0,0 +1,68 @@
+from telnetlib import DO
+from typing import Any, Text, Dict, List
+from xxlimited import foo
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa.core.channels.channel import InputChannel
+
+import random
+
+DATABASE = ['Bun bo','Mi quang','Banh canh','Com suon',
+            'Hu tieu','Goi cuon','An hai','An nan','An chay',
+            'An gi cung duoc','BeefSteak','Hotpot','Grill']
+
+
+class MyIO(InputChannel):
+    def name() -> Text:
+        "Mini Bot"
+        return "myBot"
+
+class action_save_cust_info(Action):
+    def name(self):
+        return 'action_save_cust_info'
+
+    def run(self, dispatcher, tracker, domain):
+        user_id = (tracker.current_state())["sender_id"]
+        print(user_id)
+        cust_name = next(tracker.get_latest_entity_values("cust_name"), None)
+        cust_sex = next(tracker.get_latest_entity_values("cust_sex"), None)
+        bot_position = "SHB"
+
+        if (cust_sex is  None):
+            cust_sex = "Quý khách"
+
+        if (cust_sex == "anh") | (cust_sex == "chị"):
+           bot_position = "em"
+        elif (cust_sex == "cô") | (cust_sex == "chú"):
+            bot_position = "cháu"
+        else:
+            cust_sex = "Quý khách"
+            bot_position = "SHB"
+
+        if not cust_name:
+            #dispatcher.utter_template("utter_greet_name",tracker)
+            return []
+
+        print (name_cap(cust_name))
+        return [SlotSet('cust_name', " "+name_cap(cust_name)),SlotSet('cust_sex', name_cap(cust_sex)),SlotSet('bot_position', name_cap(bot_position))]
+
+class actionRecommend(Action):
+
+    def name(self) -> Text:
+        return "utter_food"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        food = []
+        for i in range(2):
+          food_number = random.randrange(len(DATABASE))
+          food.append(DATABASE[food_number])
+
+        print("food")
+
+        dispatcher.utter_message(text="I think today you can try '{}' or besides may be is '{}'. It so tasty!".format(food[0], food[1]))
+
+        return []
---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+import json
+from time import sleep
+def run_test(text):
+    url = "http://localhost:5005/webhooks/rest/webhook"
+    response = requests.post(url, data=json.dumps({"sender": "default", "message":text}))
+    print(response.json()[0]["text"])
+
+run_test("Mr. is thang")
+sleep(2)
+run_test("Mrs. is van")
